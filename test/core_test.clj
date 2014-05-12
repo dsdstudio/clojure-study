9646983ed4480a9cfbab39aa521069fcd72463ae
보롱킴 <dsdgun@gmail.com>
2014-05-12 09:19:22 +0900

    맵쪽 정리

--- test/core_test.clj
+++ test/core_test.clj
@@ -45,3 +45,43 @@
 (true? ())
 ;> false
 
+
+;; map, keyword, 구조체 
+
+;; 네가지방식 모두 같은 표현이다. 클로저에서는 쉼표 역시 공백으로 처리한다.
+(def inventors {"Lisp" "borong" "clojure" "mickey"})
+(def inventors {"Lisp" "borong", "clojure" "mickey"})
+(def inventors {"Lisp","borong", "clojure","mickey"})
+
+;; 단지 맵이기때문에 인자가 짝수인지는 확인한다. 홀수인자를 주게되면 예외가 발생한다 
+; (def inventors {"Lisp" "borong" "clojure"})
+
+; 맵의 값을 얻기위해 get함수를 사용할수 있다. 
+; (doc get)
+(get inventors "Lisp" "엄훠 값이 없엉")
+;> "borong"
+
+; 키워드는 콜론으로 시작한다, 또한, 평가하는경우 자기자신을 반환. 
+:foo
+;> :foo
+
+; 키워드를 통해서도 맵을 만들수있다. 당연한 얘기겠지만  get할때 키워드를 키로 사용해서 값을 가져올수있다. 
+(def inventors {:Lisp "borong"})
+(inventors :Lisp)
+;> "borong"
+(inventors "Lisp")
+;> nil
+
+; 키워드는 구조체의 속성이 될수있다. 구조체를 만들때는 defstruct를 이용하여 선언할수있다능 
+(defstruct people :firstName :lastName)
+
+; 구조체를 선언한뒤 struct 함수를 이용해서 구조체의 인스턴스를 만들수 있다 
+(def borong (struct people "borong" "kim"))
+
+(:firstName borong)
+;> "borong"
+
+; struct-map 함수를 통해서 구조체를 초기화 하면 원래속성을 대신할 값을 설정하거나, 원래 속성에 없는 값을 추가해서 인스턴스를 만들수 있다 
+
+(struct-map people :age 30)
+;> {:firstName nil, :lastName nil, :age 30)
